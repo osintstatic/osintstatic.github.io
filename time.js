162ae263db178581b876715b44d7080b8474d43e
osintstatic <pichenka22866@gmail.com>
2025-02-08 07:08:56 +0500

    Update time.js

--- time.js
+++ time.js
@@ -1,20 +1,25 @@
 setInterval(function() {
     const d = new Date();
+    const options = { timeZone: 'Europe/Moscow' };
+    
     const dayElement = document.getElementById("day");
     const weekElement = document.getElementById("week");
     const hoursElement = document.getElementById("hours");
     const minutesElement = document.getElementById("minutes");
     const secondsElement = document.getElementById("seconds");
 
-    const day = d.getDate();
+    // Получаем московское время
+    const moscowDate = new Date(d.toLocaleString('en-US', options));
+
+    const day = moscowDate.getDate();
     const week = Math.floor(day / 7) + 1; // Номер текущей недели
-    const hours = d.getHours();
-    const minutes = d.getMinutes();
-    const seconds = d.getSeconds();
+    const hours = moscowDate.getHours();
+    const minutes = moscowDate.getMinutes();
+    const seconds = moscowDate.getSeconds();
 
     dayElement.textContent = `| Day - ${day}`;
     weekElement.textContent = `| Week - ${week}`;
     hoursElement.textContent = `| Hours - ${hours}`;
     minutesElement.textContent = `| Minutes - ${minutes}`;
     secondsElement.textContent = ` | Seconds - ${seconds}`;
-}, 1000); 
+}, 1000);
